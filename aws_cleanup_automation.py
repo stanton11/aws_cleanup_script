fbfa1bd36acda6a5b867611a0f1fba86ad9494d6
Mike <mike.stanton@couchbase.com>
2022-10-10 16:48:34 +0100

    Added script

---
+++ aws_cleanup_automation.py
@@ -0,0 +1,29 @@
+import boto3
+
+# create etc3 client
+ec2 = boto3.client('ec2')
+
+# retrieve all ec2 
+response = ec2.describe_addresses()
+
+# list of dictionaries relating to each address
+addresses = response['Addresses']
+
+# release any addresses with no association ID
+for address in addresses:
+    if 'AssociationId' not in address:
+        response = ec2.release_address(AllocationId=address['AllocationId'])
+        print(response)
+        print()
+
+# retrieve all ec2 network interfaces
+response = ec2.describe_network_interfaces()
+
+# list of dictionaries relating to each interface
+interfaces = response['NetworkInterfaces']
+
+# release any interfaces that are not in use
+for interface in interfaces:
+    if interface.get('Status') == 'available':
+        response = ec2.delete_network_interface(NetworkInterfaceId=interface['NetworkInterfaceId'])
+        print(response)
